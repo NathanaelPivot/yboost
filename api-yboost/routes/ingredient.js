28aa2cbf66c198bba2de449ae8cb8254361d8fd1
Epikdimond-N <nathanael.pivot@ynov.com>
2025-05-14 09:15:32 +0200

    feat(all): update commit

--- api-yboost/routes/ingredient.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getIngredientsByCocktailId } = require("../controllers/ingredientController");
-
-const router = express.Router();
-
-router.get("/:id", getIngredientsByCocktailId);
-
-module.exports = router;
